931cc8de594d9f15b63cf5bbfb3cfc42f72317cb
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2023-07-20 23:34:30 +0100

    Create middleware.js

---
+++ packages/jest/redux/middleware.js
@@ -0,0 +1,21 @@
+/**
+ * @SEE: https://redux.js.org/usage/writing-tests#middleware
+ */
+
+const thunk = ({ dispatch, getState }) => next => action => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState);
+  }
+  return next(action)
+}
+
+export createThunkEnv = () => {
+  const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn()
+  };
+  const next = jest.fn();
+  const invoke = action => thunk(store)(next)(action)
+
+  return { store, next, invoke }
+};

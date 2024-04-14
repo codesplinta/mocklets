34a51f40c1cc1ecdd77e30f4af7b17a7632f4ca9
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2024-04-14 09:24:27 +0100

    Update index.js

--- packages/jest/winston/index.js
+++ packages/jest/winston/index.js
@@ -1,24 +1,22 @@
-const logger = {
-  debug: jest.fn(),
-  log: jest.fn()
-};
-
-const $winston = {
-  format: {
-    colorize: jest.fn(),
-    combine: jest.fn(),
-    label: jest.fn(),
-    timestamp: jest.fn(),
-    printf: jest.fn()
-  },
-  createLogger: jest.fn().mockReturnValue(logger),
-  transports: {
-    Console: jest.fn()
-  }
-};
+export const fakeWinstonLoggerPackageFactory = () => {
+  const logger = {
+    debug: jest.fn(),
+    log: jest.fn()
+  };
   
-jest.mock("winston", () => ($winston));
+  const $winston = {
+    format: {
+      colorize: jest.fn(),
+      combine: jest.fn(),
+      label: jest.fn(),
+      timestamp: jest.fn(),
+      printf: jest.fn()
+    },
+    createLogger: jest.fn().mockReturnValue(logger),
+    transports: {
+      Console: jest.fn()
+    }
+  };
 
-export {
-  $winston
-}
+  return $winston;
+};

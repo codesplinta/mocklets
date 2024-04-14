eace6b7bcda7f38a1581ed097323fe904033a848
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2024-04-14 09:17:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,85 @@
+/**
+ * Validates  properties on the `window` obbject that can be overriden.
+ *
+ * @param {String} property
+ * @throws {Error}
+ *
+ * @returns void
+ */
+function assertReadonlyGlobalsNotMutable(property) {
+	const readOnlyGlobalObjects = [
+		'origin',
+		'history',
+		'clientInformation',
+		'caches',
+		'closed',
+		'crypto',
+		'fetch'
+	]
+
+	if (readOnlyGlobalObjects.includes(property)) {
+		throw new Error(
+			`Cannot override sensitive readonly global object: "${property}"`
+		)
+	}
+}
+
+/**
+ * A helper utility for replacing native object and BOM APIs in web browsers
+ * with a fake implementation replica so as to make testing a lot easier.
+ *
+ * @param {String} property
+ * @param {*} fakeOrMock
+ *
+ * @returns void
+ */
+export const provisionFakeWebPageWindowObject = (property, fakeOrMock) => {
+	const { [property]: originalProperty } = window
+
+	beforeAll(() => {
+		assertReadonlyGlobalsNotMutable(property)
+		delete window[property]
+
+		Object.defineProperty(window, property, {
+			configurable: true,
+			writable: true,
+			value: fakeOrMock
+		})
+	})
+
+	afterAll(() => {
+		if (Boolean(originalProperty)) {
+			window[property] = originalProperty
+		}
+	})
+}
+
+/**
+ * A helper utility for replacing global or exported object and APIs from 
+ * Nodejs packages  with a fake implementation replica so as to make 
+ * testing a lot easier.
+ *
+ * @param {String} property
+ * @param {*} value
+ *
+ * @returns void
+ */
+export const provisionFakeNodeJSObject = (packageOrModuleName, fakeOrMock, global = true) => {
+  beforeEach(() => {
+    const packageOrModule = require(packageOrModuleName);
+
+    if (typeof fakeOrMock === "function") {
+      if (fakeOrMock.length === 0) {
+        jest.mock(packageOrModuleName, fakeOrMock);
+      }
+    } else {
+      packageOrModule.default = jest.fn(
+        () => fakeOrMock
+      );
+    }
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+  });
+}
